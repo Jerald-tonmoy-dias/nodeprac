4bd0d4deba7cada6ca6565a6e1d1957425cddc61
Jerald Tonmoy Dias <tonmoyjareld@gmail.com>
2025-11-09 11:15:35 +0600

    log to http-server

--- http-server/index.js
+++ http-server/index.js
@@ -1,27 +1,44 @@
 const http = require("node:http");
+const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
-  //   res.statusCode = 200;
-  //   res.setHeader('Content-Type', 'text/plain');
-  //   res.end('Hello, World!\n');
+  const method = req.method;
+  const path = req.url;
 
-  switch (req.url) {
-    case "/":
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/plain");
-      res.end("Welcome to the Home Page!\n");
+  const log = `\n${new Date().toISOString()} - ${method} ${path}\n`;
+  fs.appendFileSync("log.txt", log, "utf8");
+
+  switch (method) {
+    case "GET":
+      {
+        switch (path) {
+          case "/":
+            return res
+              .writeHead(200, { "Content-Type": "text/plain" })
+              .end("Hello, World!");
+          case "/contact":
+            return res
+              .writeHead(200, { "Content-Type": "text/plain" })
+              .end("Contact us at");
+        }
+      }
       break;
-    case "/about":
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/plain");
-      res.end("This is the About Page.\n");
-      break;
+
+    case "POST": {
+      switch (path) {
+        case "/tweet":
+          return res
+            .writeHead(200, { "Content-Type": "text/plain" })
+            .end("tweet submitted!");
+      }
+    }
     default:
       res.statusCode = 404;
-      res.setHeader("Content-Type", "text/plain");
-      res.end("404 Not Found\n");
-      break;
   }
+
+  return res
+    .writeHead(404, { "Content-Type": "text/plain" })
+    .end("You are lost!");
 });
 
 const PORT = 3000;
