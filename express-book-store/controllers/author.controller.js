307ba8b95c559274d7442c1cb0cd1d2ba73f4214
Jerald Tonmoy Dias <tonmoyjareld@gmail.com>
2025-11-10 21:53:27 +0600

    express-book-store - author create-add-getall

---
+++ express-book-store/controllers/author.controller.js
@@ -0,0 +1,59 @@
+const authorsTable = require("../models/author.model");
+const db = require("../db");
+
+const { eq, sql } = require("drizzle-orm");
+
+exports.getAllAuthors = async function (req, res) {
+  const authors = await db.select().from(authorsTable);
+  res.json(authors);
+};
+
+exports.getAuthorById = async function (req, res) {
+  id = req.params.id;
+
+  const [author] = await db
+    .select()
+    .from(authorsTable)
+    .where((table) => eq(table.id, id))
+    .limit(1);
+
+  if (!author) {
+    return res.status(404).send(`Author with id ${id} does not exiest!`);
+  }
+  return res.json(author);
+};
+
+exports.createAuthor = async function (req, res) {
+  const { firstName, lastName, email } = req.body;
+
+  if (!firstName || firstName == "") {
+    return res.status(400).send("firstName is required");
+  }
+  if (!email || email == "") {
+    return res.status(400).send("email is required");
+  }
+
+  const [result] = await db
+    .insert(authorsTable)
+    .values({
+      firstName,
+      lastName,
+      email,
+    })
+    .returning({ id: authorsTable.id });
+
+  return res.status(201).json({
+    message: "Author created successfully!",
+    id: result.id,
+  });
+};
+
+// exports.deleteAuthorById = async function (req, res) {
+//   id = req.params.id;
+
+//   await db.delete(authorsTable).where(eq(authorsTable.id, id));
+
+//   return res.status(200).json({
+//     message: "Author deleted successfully!",
+//   });
+// };
--- express-book-store/index.js
+++ express-book-store/index.js
@@ -2,6 +2,7 @@ require("dotenv/config");
 const express = require("express");
 const { loggerMiddleware } = require("./middlewares/logger");
 const bookRouter = require("./routes/book.routes");
+const authorRouter = require("./routes/author.routes");
 
 const app = express();
 const port = 8000;
@@ -12,6 +13,7 @@ app.use(loggerMiddleware);
 
 // Routes
 app.use("/books", bookRouter);
+app.use("/authors", authorRouter);
 
 app.listen(port, () => {
   console.log(`Book store app listening at http://localhost:${port}`);
